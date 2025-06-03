3b53bf929cf88487855f6d88f17c8e9b3d9a554b
Elijah Aaron <elijahhager20@gmail.com>
2025-06-03 16:48:12 -0600

    Update init.hpp

--- main/init.hpp
+++ main/init.hpp
@@ -1,1 +1,13 @@
+#pragma once
 
+class Initialization{
+    public:
+        static void mainInit();
+        static void firstTimeInit();
+};
+
+class createRepos{
+    public:
+        static void loadRepos();
+        static void createFirstRepo();
+};
