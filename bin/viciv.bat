23770323ca2ec0646182712955adc079fafae5aa
Elijah Aaron <elijahhager20@gmail.com>
2025-06-16 19:02:31 -0600

    Update viciv.bat

--- bin/viciv.bat
+++ bin/viciv.bat
@@ -1,3 +1,4 @@
+echo "Vici: the one and only worst version control system known to man\nI salute you for even attempting to use this mashup of c++ header files I barely know how to use.\nAnyways, I hope you have fun using Vici as your version control system\n\n"
 @echo off
 vici.exe
 pause >nul
