79570f5ded1f8ca15cccc97c4fed3b8d8470ab4c
elijahdaboy <elijahhager20@gmail.com>
2025-06-16 21:43:40 -0600

    Names fixed to prioritize the bat file over exe

--- bin/vici.bat
+++ bin/vici.bat
@@ -5,5 +5,5 @@ echo Vici: the one and only worst version control system known to man
 echo I salute you for even attempting to use this mashup of c++ header files I barely know how to use.
 echo Anyways, I hope you have fun using Vici as your version control system!
 echo.
-vici.exe
+viciw.exe
 pause >nul
