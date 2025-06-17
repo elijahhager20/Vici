c21af1e57309b3789a350e97b8cb5709cc1a34c6
elijahdaboy <elijahhager20@gmail.com>
2025-06-16 19:10:38 -0600

    cmon man, we can talk about this

--- bin/viciv.bat
+++ bin/viciv.bat
@@ -1,4 +1,9 @@
-echo "Vici: the one and only worst version control system known to man\nI salute you for even attempting to use this mashup of c++ header files I barely know how to use.\nAnyways, I hope you have fun using Vici as your version control system!\n\n"
+:: Warning: This script was written by a sleep-deprived genius. Run at your own risk.
+
 @echo off
+echo Vici: the one and only worst version control system known to man
+echo I salute you for even attempting to use this mashup of c++ header files I barely know how to use.
+echo Anyways, I hope you have fun using Vici as your version control system!
+echo.
 vici.exe
 pause >nul
