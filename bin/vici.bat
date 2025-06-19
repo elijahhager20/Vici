f2c035dfc5557aa00c068d90591affa2cfef5a71
elijahdaboy <elijahhager20@gmail.com>
2025-06-19 10:09:16 -0600

    q

--- bin/vici.bat
+++ bin/vici.bat
@@ -1,6 +1,7 @@
 :: Warning: This script was written by a sleep-deprived genius. Run at your own risk.
 
 @echo off
+echo Use viciw.exe to ignore this message:
 echo Vici: the one and only worst version control system known to man
 echo I salute you for even attempting to use this mashup of c++ header files I barely know how to use.
 echo Anyways, I hope you have fun using Vici as your version control system!
