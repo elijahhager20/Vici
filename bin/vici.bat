22ea01b97c55b759c804e179e31fb1844fee0af0
elijahdaboy <elijahhager20@gmail.com>
2025-06-19 10:10:29 -0600

    q

--- bin/vici.bat
+++ bin/vici.bat
@@ -2,7 +2,7 @@
 
 @echo off
 echo Use viciw.exe to ignore this message:
-echo Vici: the one and only worst version control system known to man
+echo Vici: the one and only worst version control system known to man.
 echo I salute you for even attempting to use this mashup of c++ header files I barely know how to use.
 echo Anyways, I hope you have fun using Vici as your version control system!
 echo.
