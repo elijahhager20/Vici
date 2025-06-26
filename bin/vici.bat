75d64d99ac2fa929f306d5d143d607252156a063
elijahdaboy <elijahhager20@gmail.com>
2025-06-25 19:41:14 -0600

    q

--- bin/vici.bat
+++ bin/vici.bat
@@ -1,5 +1,6 @@
 @echo off
 
+:: motd
 echo Vici: the one and only worst version control system known to man.
 echo I salute you for even attempting to use this mashup of C++ header files I barely know how to use.
 echo.
